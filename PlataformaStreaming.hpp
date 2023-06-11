1a86e7d0c97cf6485d186c5aee5e1106fdb7eb8b
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-11 15:16:52 -0600

    Actualicé PlataformaStreaming.hpp
    Añadí atributos y métodos

--- PlataformaStreaming.hpp
+++ PlataformaStreaming.hpp
@@ -7,16 +7,35 @@ Jocelyn Ileana Balderas Sánchez
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include "Pelicula.hpp"
+#include "Serie.hpp"
+#include "Video.hpp"
 
 using namespace std;
 
 class PlataformaStreaming
 {
-private:
+    private:
+    vector<Serie> Series;
+    vector<Pelicula> Peliculas;
     string Compania{""};
-
-public:
+    public:
+    PlataformaStreaming();
     string getCompania();
     void setCompania(string);
+    void iniciar();
+    void leerArchivo();
+    void resumen();
+    void imprimirGenero(string gen);
+    void mostrar_apartir_cal(double rat);
+    void mostrarEpisodios_Serie_Calificacion(string nom, double rat);
+    void mostrarPelicula_Calificacion(double rat);
+    void mostrarPeliculas();
+    void mostrarSeries();
+    void mostrarEpsSerie(string nombreSerie);
+    void calificarVideoEsp(string nom, double rat);
+    void ultiMensaje();
 };
+
 #endif
